ffa242ebc881a97e6680fc4d74019f043ded14de
Ovais09 <ovazeem100@gmail.com>
2022-11-28 20:17:49 -0500

    fixex cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { db } from './firebaseconfig.js';
-import { collection, query, doc, getDocs, setDoc, deleteDoc } from 'firebase/firestore';
+import { collection, query, doc, getDocs, setDoc, deleteDoc } from 'firebase/firestore'; \
+import cors from 'cors'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 
 
