eb48d3fdff6fed038d62c820f536bbb0379cd488
Ovais09 <ovazeem100@gmail.com>
2022-11-30 18:52:27 -0500

    fixed id again

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -6,13 +6,22 @@ import {getAuth} from 'firebase/auth'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBvD-rKBc9StUR8m-v9AZY1z53tqn-N_8Q",
+//   authDomain: "seg4105-project.firebaseapp.com",
+//   projectId: "seg4105-project",
+//   storageBucket: "seg4105-project.appspot.com",
+//   messagingSenderId: "228854692949",
+//   appId: "1:228854692949:web:b0df07db1df4a94c76e8dc"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBvD-rKBc9StUR8m-v9AZY1z53tqn-N_8Q",
-  authDomain: "seg4105-project.firebaseapp.com",
-  projectId: "seg4105-project",
-  storageBucket: "seg4105-project.appspot.com",
-  messagingSenderId: "228854692949",
-  appId: "1:228854692949:web:b0df07db1df4a94c76e8dc"
+  apiKey: "AIzaSyBTZ0Lzd5GYoFuJOM8AkKbCKLeBCn69fj4",
+  authDomain: "practice-307db.firebaseapp.com",
+  projectId: "practice-307db",
+  storageBucket: "practice-307db.appspot.com",
+  messagingSenderId: "426622062956",
+  appId: "1:426622062956:web:f23ef33681ae63210aa349"
 };
 
 // Initialize Firebase
